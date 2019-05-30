55dd52a67ecad4a88d5f797a6786d74e741a093d
Felipe Cook <felipepcook@gmail.com>
2019-05-29 20:45:10 -0700

    Finished RandomLetterGenerator

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,13 +2,24 @@ import java.util.Random;
 
 public class Hangman {
 
-  String randomWord;
+  private String randomWord;
 
-  public static String generateRandomWords(int numberOfWords) {
+  public String generateRandomWords(int numberOfWords) {
 
     Random random = new Random();
+    char[] chars = new char[random.nextInt(8) + 3];
 
-    for (int i = 0; i < )
+
+    for (int i = 0; i < numberOfWords; i++) {
+
+      for (int j = 0; j < chars.length; j++) {
+        chars[j] = (char) random.nextInt('a' + 27);
+      }
+
+
+    }
+
+    return randomWord = chars.toString();
   }
 
 
