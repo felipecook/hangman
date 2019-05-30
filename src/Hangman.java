fcb700af5165e16fd9a358ad4904d5efc7c960b4
Felipe Cook <felipepcook@gmail.com>
2019-05-29 20:39:16 -0700

    Initial commit - creating RandomStringGenerator

---
+++ src/Hangman.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Hangman {
+
+  String randomWord;
+
+  public static String generateRandomWords(int numberOfWords) {
+
+    Random random = new Random();
+
+    for (int i = 0; i < )
+  }
+
+
+
+
+}
