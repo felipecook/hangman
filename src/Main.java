079741df55bbcdb80de4a4a0a9c2262986006ab6
Felipe Cook <felipepcook@gmail.com>
2019-06-14 19:56:32 -0700

    Added optionMenu method in Main

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,55 @@ import java.util.Scanner;
 
 public class Main {
 
+  private static Scanner scanner = new Scanner(System.in);
 
+  private static Hangman hangman = new Hangman();
 
   public static void main(String[] args) {
-    Hangman hangman = new Hangman();
+
+    boolean quit = false;
+
+    playGame();
 
 
-    Scanner scanner = new Scanner(System.in);
+    while (!quit) {
+      System.out.println("\nEnter action: (Press 0 to redisplay available choices)");
+      int choice = scanner.nextInt();
+      scanner.nextLine();
+      switch (choice) {
+        case 0:
+          printMenu();
+          break;
+        case 1:
+          playGame();
+          break;
+        case 2:
+          System.out.println("\nShutting down...");
+          quit = true;
+          break;
+      }
+    }
 
 
 
-    System.out.println("Hello and welcome to the game of Hangman");
-    System.out.println("Please enter the number of words you would like to guess:");
+
     int numberOfWords = Integer.parseInt(scanner.nextLine());
 
     String testString = hangman.generateRandomString(numberOfWords);
     System.out.println(testString);
 
+  }
+
+  private static void playGame() {
+    System.out.println("Hello and welcome to the game of Hangman");
+    System.out.println("Please enter the number of words you would like to guess:");
+  }
+
+  private static void printMenu() {
+    System.out.println("Please select your choice: ");
+    System.out.println("Press 0 to re-display the choices.");
+    System.out.println("Press 1 to play the game");
+    System.out.println("Press 2 to quit.");
   }
 
 }
